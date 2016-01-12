f5f66f5bb92b5eef1a5ca8faeba16c0036b74a36
Hasham <hash004@gmail.com>
2016-01-12 11:13:53 +0000

    Chapter 2 exercise answers from the book

---
+++ chapter_2/chessBoard.js
@@ -0,0 +1,17 @@
+function chessBoard(size){
+    var grid = size;
+    var hash = 0;
+    var board = "";
+    for(i=0;i<grid;i++){
+        for(j=0;j<grid;j++){
+            hash++;
+            if((hash+i)%2==0){
+                board += "#";
+            } else {
+                board += " ";
+            }
+        }
+        board += "\n";
+    }
+    console.log(board);
+}
---
+++ chapter_2/loopTriangle.js
@@ -0,0 +1,10 @@
+function tri(){
+    var k ="";
+    for(i=1;i<=7;i++){
+        for(j=0;j<i;j++){
+            k +="#";
+        }
+        console.log(k);
+        k = "";
+    }
+}
